4db9ccd3bd3b6d14ef251ef70d85d9d8ac7b8f8f
Joseph Gonzalez <josephgonowak97@gmail.com>
2021-08-09 17:53:35 -0400

    First version

---
+++ multitask_queue/tests/test_decorator.py
@@ -0,0 +1,14 @@
+from multitask_queue.task import TaskDescriptor
+
+from multitask_queue import decorators
+
+
+class TestDecorators:
+
+    def test_regular_task(self):
+        pass
+
+
+if __name__ == "__main__":
+    test = TestDecorators()
+
