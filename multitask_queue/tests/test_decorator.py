888f722633c7409c4f530f1003750557b7bcd5de
Joseph Gonzalez <josephgonowak97@gmail.com>
2021-08-09 18:21:59 -0400

    Adding unitary tests for the decorators

--- multitask_queue/tests/test_decorator.py
+++ multitask_queue/tests/test_decorator.py
@@ -5,10 +5,51 @@ from multitask_queue import decorators
 
 class TestDecorators:
 
-    def test_regular_task(self):
-        pass
+    def test_decorators(self):
+        total_decorators = {
+            'regular_task': 'regular',
+            'parallel_task': 'parallel',
+            'independent_task': 'independent',
+            'async_task': 'async',
+            'async_independent_task': 'async_independent',
+            'pre_execution_task': 'pre_execution',
+            'autofill_task': 'autofill'
+        }
+        for decorator_name, type_task in total_decorators.items():
+            decorator = getattr(decorators, decorator_name)
+
+            if decorator_name != 'autofill_task':
+                @decorator(
+                    exec_on_events=['event'],
+                    exec_after_tasks=['dummy'],
+                    exec_before_tasks=['dummy2'],
+                    autofill=['dummy3']
+                )
+                def dummy_func(a: int = 5):
+                    pass
+
+                task = decorators.PLUGINS['dummy_func']
+                assert task.func == dummy_func
+                assert task.exec_on_events == {'event'}
+                assert task.exec_after_tasks == {'dummy'}
+                assert task.exec_before_tasks == {'dummy2'}
+                assert task.autofill == {'dummy3'}
+
+            else:
+                @decorator
+                def dummy_func(a: int = 5):
+                    pass
+
+                task = decorators.PLUGINS['dummy_func']
+
+            assert task.type_task == type_task
+            assert task.parameters == ['a']
+            assert task.default_parameters == {'a': 5}
+
+            del decorators.PLUGINS['dummy_func']
 
 
 if __name__ == "__main__":
     test = TestDecorators()
+    test.test_decorators()
 
